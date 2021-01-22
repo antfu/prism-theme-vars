bb4edadec0cc2b987da025b9f33d847bc05ee770
Anthony Fu <anthonyfu117@hotmail.com>
2021-01-22 15:19:23 +0800

    chore: init

---
+++ scripts/gen.ts
@@ -0,0 +1,10 @@
+import MarkdownIt from 'markdown-it'
+import Prism from 'markdown-it-prism'
+import fs from 'fs'
+
+let html = fs.readFileSync('demo/index.html', 'utf-8')
+const md = new MarkdownIt()
+md.use(Prism)
+const rendered = md.render(fs.readFileSync('demo/index.md', 'utf-8'))
+html = html.replace(/<!--start-->[\s\S\n]*<!--end-->/m, `<!--start-->${rendered}<!--end-->`)
+fs.writeFileSync('demo/index.html', html, 'utf-8')
